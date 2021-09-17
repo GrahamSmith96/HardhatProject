42b40ba99a6f76bfd7a54479227975cf96e4dc96
GrahamSmith96 <37817205+GrahamSmith96@users.noreply.github.com>
2021-09-16 23:15:19 -0400

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,14 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/c80e74e81de64813826a06700d4f8455",
-      accounts: ['f3ffd81467dbd8fe517213d2b81c6b4b80b897de6880c8d68dfbe261a57b23ec']
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/kj7YRy6YmbNuTI1EzCKLNE25MMWii8O4",
+        blockNumber: 13234050
+      }
     }
   },
+  
   etherscan: {
     apiKey: process.env.ETHERSCAN_KEY
   },
